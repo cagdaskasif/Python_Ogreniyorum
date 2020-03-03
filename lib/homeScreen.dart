6b2a41359538e85f98893b687e5b73041ea19301
Cagdas Kasif <cagdaskasif@gmail.com>
2020-03-03 13:40:04 +0300

    ekran boyutuna göre değişen buton boyutu ayarlandı

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'dersler/giris/bolumler.dart';
-import 'kullanici_girisi.dart';
-import 'kurulum/kurulum.dart';
-import 'oyunlar/oyunlarRadioButton.dart';
 
 class homeScreen extends StatefulWidget {
   @override
@@ -11,161 +7,158 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+  final List<IconData> iconData = <IconData>[
+    Icons.school,
+    Icons.extension,
+    Icons.person,
+    Icons.laptop_windows,
+  ];
+
   @override
   Widget build(BuildContext context) {
+    double bosluk = 20.0;
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 235, 59),
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            ("Python Öğren"),
-            style: TextStyle(fontSize: 30),
-          ),
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20),
-          ),
-        ),
-        backgroundColor: Color.fromARGB(255, 112, 112, 112),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                  children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 112, 112, 112),
-                        shape: BoxShape.circle),
-                    padding: EdgeInsets.all(10),
+          backgroundColor: Color.fromARGB(255, 255, 235, 59),
+          appBar: AppBar(
 
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ogrenmeyebasla()));
-                      },
-                      icon: Icon(
-                        Icons.extension,
-                        color: Color.fromARGB(255, 255, 235, 59),
-                        size: 100,
+            title: Center(
+              child: Text(
+                ("Python Öğren"),
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(20),
+              ),
+            ),
+            backgroundColor: Color.fromARGB(255, 112, 112, 112),
+          ),
+          body: OrientationBuilder(builder: (context, telyonu) {
+            return telyonu == Orientation.portrait
+                ? Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Kutu(context,"Kodla", "/link1", 0),
+                              SizedBox(width: bosluk),
+                              Kutu(context,"Çözelim", "/link2", 1),
+                            ],
+                          ),
+                          SizedBox(height: bosluk),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Kutu(context,"Profil", "/link3", 2),
+                              SizedBox(width: bosluk),
+                              Kutu(context,"Kurulum", "/link4", 3),
+                            ],
+                          ),
+                          SizedBox(height: bosluk),
+                        ],
                       ),
-                    ),
-                    width: 150,
-                    height: 150,
                   ),
-                  Text(
-                    "Kodla",
-                    ),
-                ],
-              ),
-              Container(
-                //color: Colors.blue,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 112, 112, 112),
-                          shape: BoxShape.circle),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => OyunlarRadioButton()));
-                        },
-                        icon: Icon(
-                          Icons.videogame_asset,
-                          color: Color.fromARGB(255, 255, 235, 59),
-                          size: 100,
-                        ),
+                )
+                : Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Kutu2(context,"Buton1","/link1", 0),
+                              SizedBox(width: bosluk),
+                              Kutu2(context,"Buton2", "/link2", 1),
+                            ],
+                          ),
+                          SizedBox(height: bosluk),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Kutu2(context,"Buton3", "/link3", 2),
+                              SizedBox(width: bosluk),
+                              Kutu2(context,"Buton4", "/link4", 3),
+                            ],
+                          ),
+                          SizedBox(height: bosluk),
+                        ],
                       ),
-                      width: 150,
-                      height: 150,
-                    ),
-                    Text(
-                      "Çözelim",
-                    ),
-                  ],
-                ),
+                  ),
+                );
+          })
+    );
+  }
+
+  Kutu(context,isim, link, iconNo) {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 112, 112, 112),
+              shape: BoxShape.circle),
+          width: (MediaQueryData.fromWindow(window).size.width) * 0.3,
+          //width: MediaQuery.of(context).size.width;
+          height: (MediaQueryData.fromWindow(window).size.width) * 0.3,
+          //color: Color(renk),
+          child: Center(
+            child: IconButton(
+              // icon: Icon(Icons.ac_unit),
+              icon: Icon(
+                iconData[iconNo],
+                color: Color.fromARGB(255, 255, 235, 59),
               ),
-            ],
+              onPressed: (){
+                Navigator.pushReplacementNamed(context, "$link");
+              },
+              iconSize: MediaQueryData.fromWindow(window).size.width * 0.2,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                // color: Colors.white,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 112, 112, 112),
-                        borderRadius: BorderRadius.circular(90),
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginPage2()));
-                        },
-                        icon: Icon(
-                          Icons.person,
-                          color: Color.fromARGB(255, 255, 235, 59),
-                        ),
-                        iconSize: 100,
-                      ),
-                      width: 150,
-                      height: 150,
-                    ),
-                    Text(
-                      "Profil",
-                    ),
-                  ],
-                ),
+        ),
+        Text(
+          isim,
+          style: TextStyle(
+              fontSize: MediaQueryData.fromWindow(window).size.width * 0.05),
+        )
+      ],
+    );
+  }
+
+  Kutu2(context,isim, link, iconNo) {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 112, 112, 112),
+              shape: BoxShape.circle),
+          width: (MediaQueryData.fromWindow(window).size.height) * 0.25,
+          //width: MediaQuery.of(context).size.width;
+          height: (MediaQueryData.fromWindow(window).size.height) * 0.25,
+          //color: Color(renk),
+          child: Center(
+            child: IconButton(
+              // icon: Icon(Icons.ac_unit),
+              icon: Icon(
+                iconData[iconNo],
+                color: Color.fromARGB(255, 255, 235, 59),
               ),
-              Container(
-                // color: Colors.black,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 112, 112, 112),
-                        borderRadius: BorderRadius.circular(90),
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Kurulum()));
-                        },
-                        icon: Icon(
-                          Icons.laptop_windows,
-                          color: Color.fromARGB(255, 255, 235, 59),
-                          size: 100,
-                        ),
-                      ),
-                      width: 150,
-                      height: 150,
-                    ),
-                    Text(
-                      "Kurulum",
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              onPressed: (){
+                Navigator.pushReplacementNamed(context, "$link");
+              },
+              iconSize: MediaQueryData.fromWindow(window).size.height * 0.15,
+            ),
           ),
-        ],
-      ),
+        ),
+        Text(
+          isim,
+          style: TextStyle(
+              fontSize: MediaQueryData.fromWindow(window).size.height * 0.05),
+        )
+      ],
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dersler/giris/bolumler.dart';
 import 'homeScreen.dart';
+import 'kullanici_girisi.dart';
+import 'kurulum/kurulum.dart';
+import 'oyunlar/oyunlarRadioButton.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/link1':(context)=>ogrenmeyebasla(),
+        '/link2':(context)=>OyunlarRadioButton(),
+        '/link3':(context)=>LoginPage2(),
+        '/link4':(context)=>Kurulum(),
+      },
       title: 'Python Öğren',
       theme: ThemeData(
         textTheme: GoogleFonts.sourceCodeProTextTheme(
